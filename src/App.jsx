61579727aaeea701365ed9a3d0110e04f3de5549
kcstarless <155853062+kcstarless@users.noreply.github.com>
2025-02-15 09:46:18 +1100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <header>header</header>
+      <nav>nav</nav>
+      <main>body</main>
+      <footer>footer</footer>
+    </>
+  );
+}
+
+export default App;
